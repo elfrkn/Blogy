0ea4199e3760e3684ad37d8781924ec4c6c04e71
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-02-28 21:56:34 +0100

    Add project files.

---
+++ Blogy.DataAccessLayer/Context/BlogyContext.cs
@@ -0,0 +1,25 @@
+﻿using Blogy.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogy.DataAccessLayer.Context
+{
+    public class BlogyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MSI;initial catalog=DbBlogy;integrated security=true;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
